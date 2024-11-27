351373fca59fe41c4372fa465f4c8915c436f1c0
Srikar Reddy Karemma <36558447+KSR4599@users.noreply.github.com>
2024-11-28 05:12:29 +0530

    Update campaign.js

--- ethereum/campaign.js
+++ ethereum/campaign.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import Campaign from './build/Campaign.json';
 
-//This file is only meant to return contract based on the address provided.
+//This file is only meant to return contract based on the address provided..
 export default (address) => {
   return new web3.eth.Contract (
     JSON.parse(Campaign.interface),
